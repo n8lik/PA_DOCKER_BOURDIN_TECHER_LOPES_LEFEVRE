8fc5e7f01c35d105aca1c4d72fedf32bde3081f6
kyllian <kyllian.lefevre@icloud.com>
2024-07-04 01:19:07 +0200

    commit

--- API/database/settings.php
+++ API/database/settings.php
@@ -3,6 +3,6 @@
 define("DB_PREFIX","house_");
 define("DB_NAME",getenv("DB_NAME"));
 define("DB_USER",getenv("DB_USER"));
-define("DB_PWD",fread(fopen(getenv("DB_PASSWORD_FILE"), "r");, filesize(getenv("DB_PASSWORD_FILE"))));
+define("DB_PWD",fread(fopen(getenv("DB_PASSWORD_FILE"), "r"), filesize(getenv("DB_PASSWORD_FILE"))));
 define("DB_PORT","3306");
 define("DB_HOST","localhost");
