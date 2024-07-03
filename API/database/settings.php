a6bafb2098695730ec968a52af4f7076c8ca7772
kyllian <kyllian.lefevre@icloud.com>
2024-07-04 01:34:43 +0200

    fez

--- API/database/settings.php
+++ API/database/settings.php
@@ -5,4 +5,4 @@ define("DB_NAME",getenv("DB_NAME"));
 define("DB_USER",getenv("DB_USER"));
 define("DB_PWD",fread(fopen(getenv("DB_PASSWORD_FILE"), "r"), filesize(getenv("DB_PASSWORD_FILE"))));
 define("DB_PORT","3306");
-define("DB_HOST","localhost");
+define("DB_HOST","127.0.0.1");
