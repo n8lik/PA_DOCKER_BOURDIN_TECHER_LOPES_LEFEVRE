aeb888e62a5d77210aaa3df9375c2e86f248be00
n8lik <kyllian.lefevre@icloud.com>
2024-07-04 18:55:04 +0200

    fdsuhf

--- API/database/settings.php
+++ API/database/settings.php
@@ -1,13 +1,13 @@
 <?php
 
 define("DB_PREFIX","house_");
-// define("DB_NAME",getenv("DB_NAME"));
-// define("DB_USER","getenv("DB_USER")");
-// define("DB_PWD","fread(fopen(getenv("DB_PASSWORD_FILE"), "r"), filesize(getenv("DB_PASSWORD_FILE")))");
+define("DB_NAME",getenv("DB_NAME"));
+define("DB_USER",getenv("DB_USER"));
+define("DB_PWD",fread(fopen(getenv("DB_PASSWORD_FILE"), "r"), filesize(getenv("DB_PASSWORD_FILE"))));
 
-define("DB_NAME","pcs_all_bdd");
-define("DB_USER","serveur");
-define("DB_PWD","serveur");
+#define("DB_NAME","pcs_all_bdd");
+#define("DB_USER","serveur");
+#define("DB_PWD","serveur");
 
 define("DB_PORT","3306");
 define("DB_HOST","mysql");
